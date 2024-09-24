e7a585ba6818695c03e3f080674074c648523c8d
ebenofere <eofere@gmail.com>
2024-09-24 01:21:25 -0400

    setup for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { css } from '../../styled-system/css';
  
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold', color:"teal" })}>Hello 🐼!</div>
+    <div className={css({background: "#071B31"})}>
+      <div className={css({ fontSize: "2xl", fontWeight: 'bold', color:"#09A5DB" })}>Welcome to Medconet</div>
+    </div>
   )
 }
