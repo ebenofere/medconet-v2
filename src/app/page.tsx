1e1775a3fa2c2304aed1498a9843be0fa009a84c
ebenofere <eofere@gmail.com>
2024-09-24 01:48:01 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
-import { css } from '../../styled-system/css';
+import localFont from "next/font/local";
+import Header from "@/templates/homepage/Header";
  
+const EuclidCircular = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Euclid-Circular-A-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Euclid-Circular-A-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Euclid-Circular-A-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Euclid-Circular-A-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+});
+
 export default function Home() {
   return (
-    <div className={css({background: "#071B31"})}>
-      <div className={css({ fontSize: "2xl", fontWeight: 'bold', color:"#09A5DB" })}>Welcome to Medconet</div>
-    </div>
+    <main className={EuclidCircular.className}>
+      <div>
+          <Header />
+      </div>
+    </main>
   )
 }
+
+
+{/* <div className={css({ fontSize: "2xl", fontWeight: 'bold', color:"#09A5DB" })}>Welcome to Medconet</div> */}
