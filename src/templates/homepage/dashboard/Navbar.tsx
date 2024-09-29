8165ad7ba86951d67e3dfd445a95d18700ff1627
ebenofere <eofere@gmail.com>
2024-09-29 18:36:06 -0400

    update

--- src/templates/homepage/dashboard/Navbar.tsx
+++ src/templates/homepage/dashboard/Navbar.tsx
@@ -1,10 +1,12 @@
+
+import { css } from "../../../../styled-system/css"
 import { Box } from "../../../../styled-system/jsx"
 import Search from "./Search"
 
 
 export const Navbar = () => {
   return (
-    <div>
+    <div  className={css({border: "1px solid black", bg:"yellow.100"})}>
         <div>
           <Box>Medconet</Box>
         </div>
